dc7fbd24e3b304adffcb5713b2ce63f7e29a8476
AYOUB <demo65575@gmail.com>
2024-10-30 05:14:44 +0100

    ex01

---
+++ addTwoNumbers.java
@@ -0,0 +1,11 @@
+public class addTwoNumbers {
+
+    public static void main(String[] args) {
+        final String message = "Bienvenue dans la programmation Java!";
+
+        System.out.println(message);
+    }
+
+
+
+}
